7d64157f35d820a64226f0a3cf09eb298379cadf
Richard Gong <richardgong@gmail.com>
2020-10-09 00:00:42 -0700

    make it work

---
+++ test_gong.py
@@ -0,0 +1,47 @@
+import imageio
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from skimage.transform import resize
+from IPython.display import HTML
+import warnings
+warnings.filterwarnings("ignore")
+
+source_image = imageio.imread('./first-order-motion-model/02.png')
+reader = imageio.get_reader('./first-order-motion-model/04.mp4')
+
+
+#Resize image and video to 256x256
+
+source_image = resize(source_image, (256, 256))[..., :3]
+
+fps = reader.get_meta_data()['fps']
+driving_video = []
+try:
+    for im in reader:
+        driving_video.append(im)
+except RuntimeError:
+    pass
+reader.close()
+
+driving_video = [resize(frame, (256, 256))[..., :3] for frame in driving_video]
+
+def display(source, driving, generated=None):
+    fig = plt.figure(figsize=(8 + 4 * (generated is not None), 6))
+
+    ims = []
+    for i in range(len(driving)):
+        cols = [source]
+        cols.append(driving[i])
+        if generated is not None:
+            cols.append(generated[i])
+        im = plt.imshow(np.concatenate(cols, axis=1), animated=True)
+        plt.axis('off')
+        ims.append([im])
+
+    ani = animation.ArtistAnimation(fig, ims, interval=50, repeat_delay=1000)
+    plt.close()
+    return ani
+
+
+HTML(display(source_image, driving_video).to_html5_video())
